6d0f76e6c24a8450d8a46c1c2e6b741c88135ab1
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2017-04-07 10:50:39 +0900

    Code cleanup

--- client/components/App.vue
+++ client/components/App.vue
@@ -14,7 +14,7 @@ export default {
   },
   computed: {
     bgClass () {
-      return 'bg' + (this.bgId % 5 + 1)
+      return 'bg' + (this.bgId % 6 + 1)
     }
   },
   created () {
@@ -35,43 +35,22 @@ body {
   position: fixed;
   width: 100%;
   height: 100%;
-  left: 0;
-  top: 0;
-  background-image: url(../assets/bg5.jpg);
-  background-size: cover;
-  background-repeat: no-repeat;
   margin: 0;
   padding: 0;
+  left: 0;
+  top: 0;
+  background: url(../assets/bg5.jpg) cover no-repeat;
   transition: background 0.5s ease-in-out;
-}
-#app.bg1 {
-  background-image: url(../assets/bg1.jpg);
-}
-#app.bg2 {
-  background-image: url(../assets/bg2.jpg);
-}
-#app.bg3 {
-  background-image: url(../assets/bg3.jpg);
-}
-#app.bg4 {
-  background-image: url(../assets/bg4.jpg);
-}
-#app.bg5 {
-  background-image: url(../assets/bg5.jpg);
-}
-#app.bg6 {
-  background-image: url(../assets/bg6.jpg);
+
+  .bg1 { background-image: url(../assets/bg1.jpg); }
+  .bg2 { background-image: url(../assets/bg2.jpg); }
+  .bg3 { background-image: url(../assets/bg3.jpg); }
+  .bg4 { background-image: url(../assets/bg4.jpg); }
+  .bg5 { background-image: url(../assets/bg5.jpg); }
+  .bg6 { background-image: url(../assets/bg6.jpg); }
 }
 h1 {
   text-align: center;
 }
-.page {
-  text-align: center;
-  /* nesting for the need to test postcss */
-  code {
-    background-color: #f0f0f0;
-    padding: 3px 5px;
-    border-radius: 2px;
-  }
-}
+
 </style>
